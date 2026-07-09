e37b5bf3e517556adc90cea82cb750b9f003b0a5
Kingsley Ezenwere <kingsley.ezenwere@gmail.com>
2026-07-09 22:31:09 +0100

    upgrade vercel.json #4

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,4 +1,13 @@
 """
     Vercel adaptation
 """
+
+from pathlib import Path
+
+print("Current file:", __file__)
+print("Current cwd:", Path.cwd())
+
+for p in Path("/vercel/path1").rglob("firebase-service-account.json"):
+    print("Found:", p)
+    
 from veriprops import app
